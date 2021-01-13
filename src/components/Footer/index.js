a60a69df2f465d15cbf861b3ebcb0d6fdd150408
1997808 <khanhhuyle22@gmail.com>
2021-01-14 01:41:14 +0700

    newRelease Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,58 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Link,
+} from "react-router-dom";
+import Logo from './logo.svg';
+
+{/* <Link to="/topics">Topics</Link> */ }
 
 export default function Footer() {
-  return <h2>Footer</h2>;
+  return (
+    <div className="container mx-auto w-full pt-20 pb-16">
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-3">
+          <img src={Logo} alt="logo" />
+          <p className="text-sm pt-5">Chăm sóc khách hàng: 1900-6035</p>
+          <p className="text-sm pt-5">Hỗ trợ khách hàng: hotro@bookworm.vn</p>
+          <p className="text-sm pt-5">Báo lỗi bảo mật: security@bookworm.vn</p>
+        </div>
+
+        <div className="col-span-9 grid grid-cols-4 gap-4">
+          <div className="col-span-1">
+            <p className="text-lg font-medium pb-0.5">Về chúng tôi</p>
+            <p className="text-sm pt-5">Giới thiệu</p>
+            <p className="text-sm pt-5">Liên hệ</p>
+            <p className="text-sm pt-5">Hợp tác</p>
+            <p className="text-sm pt-5">Tuyển dụng</p>
+          </div>
+
+          <div className="col-span-1">
+            <p className="text-lg font-medium pb-0.5">Hỗ trợ khách hàng</p>
+            <p className="text-sm pt-5">Câu hỏi thường gặp</p>
+            <p className="text-sm pt-5">Gửi yêu cầu hỗ trợ</p>
+            <p className="text-sm pt-5">Hướng dẫn đặt hàng</p>
+            <p className="text-sm pt-5">Phường thức vận chuyển</p>
+            <p className="text-sm pt-5">Báo lỗi bảo mật</p>
+          </div>
+
+          <div className="col-span-1">
+            <p className="text-lg font-medium pb-0.5">Chính sách</p>
+            <p className="text-sm pt-5">Chính sách đổi trả</p>
+            <p className="text-sm pt-5">Chính sách bảo mật</p>
+            <p className="text-sm pt-5">Chính sách thanh toán</p>
+          </div>
+
+          <div className="col-span-1">
+            <p className="text-lg font-medium pb-0.5">Danh mục</p>
+            <p className="text-sm pt-5">Văn học</p>
+            <p className="text-sm pt-5">Kỹ năng sống</p>
+            <p className="text-sm pt-5">Giáo dục</p>
+            <p className="text-sm pt-5">Tiểu thuyết</p>
+            <p className="text-sm pt-5">Khác</p>
+          </div>
+        </div>
+      </div>
+    </div >
+  )
 }
--- src/components/NewRelease/index.js
+++ src/components/NewRelease/index.js
@@ -1,5 +1,29 @@
-import React from "react"
+import {
+  BrowserRouter as Router,
+  Link,
+} from "react-router-dom";
+import BookSmall from "../bookSmall"
+import newBookBanner from '../../images/newBook.webp';
+import bookdemo from '../../images/bookdemo.png';
 
-export default function NewRealease() {
-  return <h2>NewRealease</h2>;
+export default function NewRelease() {
+  return (
+    <div className="container mx-auto w-full pt-20">
+      <div className="flex justify-between items-center">
+        <p className="text-3xl">Sách mới phát hành</p>
+        <p>Xem thêm</p>
+      </div>
+      <div className="w-full flex pt-8 items-stretch">
+        <img src={newBookBanner} alt="banner" className="w-2/5 flex-grow object-cover" />
+        <div className="w-3/5 grid grid-cols-3">
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+          <BookSmall photo={bookdemo} name="Lối sống tối giản của người Nhật" author="Sasaki Fumio" price="56.000 VNĐ" />
+        </div>
+      </div>
+    </div>
+  )
 }
