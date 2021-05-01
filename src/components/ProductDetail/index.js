32d12e0bbcc9a6c27ee56ccc1f3d4434364220bf
1997808 <khanhhuyle22@gmail.com>
2021-05-01 17:46:45 +0700

    add to cart client

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -1,22 +1,28 @@
-import { bookData } from "../../assets/book"
+import { bookData } from "../../assets/book";
 
 export default function ProductDetail({ id }) {
-  const data = bookData.filter(items => items.id === id)[0]
+  const data = bookData.filter((items) => items.id === id)[0];
 
   return (
     <div className="container mx-auto w-full mt-10 flex flex-grow border-t border-b border-solid border-gray-200">
       <div className="w-4/12 p-10 border-r border-solid border-gray-200 flex justify-center items-center">
-        <img src={`${process.env.PUBLIC_URL}${data.image}`} className="w-full h-80 object-scale-down" alt="book" />
+        <img
+          src={`${process.env.PUBLIC_URL}${data.image}`}
+          className="w-full h-80 object-scale-down"
+          alt="book"
+        />
       </div>
       <div className="w-8/12 p-10">
         <p className="text-2xl font-medium">{data.name}</p>
-        <p className="pt-6">Tác giả <span className="text-gray-400 pl-4">{data.author}</span></p>
+        <p className="pt-6">
+          Tác giả <span className="text-gray-400 pl-4">{data.author}</span>
+        </p>
         <p className="text-2xl font-medium pt-6">{data.price}</p>
         <p className="pt-6 truncate-4-lines">{data.description}</p>
-        <div className="h-12 w-48 flex justify-center items-center bg-black mt-6">
+        <div className="h-12 w-48 flex justify-center items-center bg-black mt-6 cursor-pointer">
           <p className="text-white">Thêm vào giỏ hàng</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/book.js
+++ src/components/book.js
@@ -1,17 +1,47 @@
-import {
-  Link,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Book({ id, photo, name, author, price, sale }) {
+  const [isShown, setIsShown] = useState(false);
   return (
-    <Link to={`/book-detail/${id}`}>
-      <div className="p-8 border border-solid border-gray-200">
-        <img src={`${process.env.PUBLIC_URL}${photo}`} alt="icon" className="h-48 w-full object-scale-down" />
-        {sale === "true" ? <p className="text-xs uppercase text-red-500 pt-2">Giảm giá</p> : <div className="h-4 mt-2"></div>}
-        <p className="text-base truncate-2-lines h-14">{name}</p>
-        <p className="text-xs text-gray-400 truncate-1-lines">{author}</p>
-        <p className="text-lg font-medium">{price}</p>
+    <div
+      className="p-8 border border-solid border-gray-200 relative"
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      <Link to={`/book-detail/${id}`}>
+        <img
+          src={`${process.env.PUBLIC_URL}${photo}`}
+          alt="icon"
+          className="h-48 w-full object-scale-down"
+        />
+      </Link>
+      <Link to={`/book-detail/${id}`}>
+        <div
+          className={`relative z-10 transition bg-white duration-200 ease-in-out transform ${
+            isShown ? "-translate-y-10" : ""
+          }`}
+        >
+          {sale === "true" ? (
+            <p className="text-xs uppercase text-red-500 pt-2">Giảm giá</p>
+          ) : (
+            <div className="h-4 mt-2"></div>
+          )}
+          <p className="text-base truncate-2-lines h-14">{name}</p>
+          <p className="text-xs text-gray-400 truncate-1-lines">{author}</p>
+          <p className="text-lg font-medium">{price}</p>
+        </div>
+      </Link>
+
+      <div className="absolute left-0 bottom-0 right-0 p-8">
+        <p
+          className={`transition duration-300 ease-in-out transform cursor-pointer ${
+            isShown ? "opacity-100" : "opacity-20"
+          }`}
+        >
+          Thêm vào giỏ
+        </p>
       </div>
-    </Link>
-  )
+    </div>
+  );
 }
