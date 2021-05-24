8dba1aa0255fb09d4cb63f7548c2ce27fec2f9e0
1997808 <khanhhuyle22@gmail.com>
2021-05-24 21:12:24 +0700

    no need redux dev tool

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
 const storeEnhancer = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const store = createStore(rootReducer, storeEnhancer);
